b0e7b5113c3cbb72ea34b539223b2acd74cbf3f7
Michele Gallo <michelegallo997@gmail.com>
2024-09-08 20:25:29 -0300

    PayCard_4

---
+++ app.js
@@ -0,0 +1,18 @@
+const cardNumber=document.getElementById("number");
+const numberInp=document.getElementById("card_number");
+
+const cardName=document.getElementById("name");
+const nameInp=document.getElementById("card_name");
+
+const cardMonth=document.getElementById("month");
+const monthInp=document.getElementById("card_month");
+
+const cardYear=document.getElementById("year");
+const yearInp=document.getElementById("card_year");
+
+const cardCvc=document.getElementById("cvc");
+const cvcInp=document.getElementById("card_cvc");
+
+const submitBtn=document.getElementById("submit_btn");
+
+
